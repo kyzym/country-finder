3ae0206fe7304ca09367bb0c6fff46e991b6ca9f
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2023-01-10 07:25:12 +0100

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 import * as ReactDOMClient from "react-dom/client";
-import {Provider} from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
 
-import {store} from './store';
-
+import { store } from "./store";
 
 const rootElement = document.getElementById("root") as HTMLDivElement;
 const root = ReactDOMClient.createRoot(rootElement);
@@ -15,7 +14,7 @@ const root = ReactDOMClient.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/countries-flags">
         <App />
       </BrowserRouter>
     </Provider>
